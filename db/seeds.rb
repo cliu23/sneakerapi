72dd9070c1ccf76992e639832a27fa54168e6aec
Lawrence Tran <lawrencetran1@gmail.com>
2015-05-12 15:17:29 -0700

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,44 @@
-20.times do
-Store.create(name: Faker::Name.name, address: Faker::Address.street_address, city: Faker::Address.city, state: Faker::Address.state, zipcode: Faker::Address.zip_code)
-end
+Sneaker.create!([
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  {name: "Kobe", brand: "Nike", cost: 99.99, color: "Black", store_id: 1}
+  
+])
+Store.create!([
+  {name: "Ara Hodkiewicz", address: "5615 Lincoln Mountain", city: "New Rubye", state: "Texas", zipcode: "78206"},
+  {name: "Quentin Walter II", address: "53212 Meggie Oval", city: "Joelfort", state: "Florida", zipcode: "40854"},
+  {name: "Delphine Bahringer II", address: "6546 Hand Glens", city: "East Mariamland", state: "North Carolina", zipcode: "68929"},
+  {name: "Quinton Kunde", address: "9109 Hoppe Green", city: "South Maggie", state: "South Carolina", zipcode: "69976"},
+  {name: "Lavinia Koelpin", address: "1989 Reinger Road", city: "West Helena", state: "Alabama", zipcode: "35532"},
+  {name: "Elenor Bernier", address: "7822 Armstrong Land", city: "Judsonborough", state: "North Dakota", zipcode: "49576-7274"},
+  {name: "Maggie Fay", address: "99546 Itzel Spur", city: "Mathiasfurt", state: "Virginia", zipcode: "15214-8053"},
+  {name: "Garnett Hamill Jr.", address: "63948 Halvorson Port", city: "Kleinview", state: "Utah", zipcode: "18989"},
+  {name: "Mariana Hudson", address: "4654 Brenda Radial", city: "New Hershelhaven", state: "California", zipcode: "13180"},
+  {name: "Everardo Lesch PhD", address: "68947 Katrina Stravenue", city: "Adonisfurt", state: "Illinois", zipcode: "74021"},
+  {name: "Alvah Swaniawski", address: "408 Isac Via", city: "Wiltonburgh", state: "Pennsylvania", zipcode: "33696"},
+  {name: "Vernie Frami", address: "5382 Tremblay Lodge", city: "Cartwrightberg", state: "Montana", zipcode: "17184-8904"},
+  {name: "Owen Gottlieb", address: "46961 Sadie Corner", city: "North Cynthiachester", state: "Ohio", zipcode: "32255-2076"},
+  {name: "Cristopher O'Kon", address: "38552 Bailey Island", city: "Alexismouth", state: "South Carolina", zipcode: "81682-1268"},
+  {name: "Demarcus Kertzmann", address: "7572 Hartmann Unions", city: "Kurtland", state: "Vermont", zipcode: "87563"},
+  {name: "Ms. Ned O'Kon", address: "61136 Norris Fords", city: "Ginostad", state: "Louisiana", zipcode: "69418-3089"},
+  {name: "Raleigh Armstrong", address: "1576 Tromp Circles", city: "Port Christelle", state: "Arizona", zipcode: "81909-3193"},
+  {name: "Peyton Bruen", address: "499 Gerardo Shoal", city: "Santinochester", state: "Indiana", zipcode: "34486"},
+  {name: "Dr. Marielle O'Kon", address: "60884 Cormier Keys", city: "Thompsonberg", state: "Florida", zipcode: "21303-4423"},
+  {name: "Sherwood Pouros", address: "2828 Jake Isle", city: "West Lowellview", state: "Kansas", zipcode: "84319"}
+])
